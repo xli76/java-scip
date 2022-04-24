6a16816f116b466972d564cead940d05446b4d2f
xin li <xli76@stevens.edu>
2022-04-24 16:29:38 +0800

    update executor

--- src/main/java/com/example/javascip/ScipService.java
+++ src/main/java/com/example/javascip/ScipService.java
@@ -4,14 +4,31 @@ import com.example.javascip.strategy.StrategyFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.*;
+
 @Component
 public class ScipService {
-//    @Autowired
-//    private ScipSolver solver;
+    private static final ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2);
+
     @Autowired
     private StrategyFactory strategyFactory;
 
     public String solve() {
+        return runnable();
+    }
+
+    private String runnable() {
+        Future<String> future = executor.submit(() -> doSolve());
+        String result = "";
+        try {
+            result = future.get(1/** second */, TimeUnit.SECONDS);
+        } catch (ExecutionException | InterruptedException | TimeoutException e) {
+            future.cancel(true);
+        }
+        return result;
+    }
+
+    private String doSolve() {
         Solver solver = strategyFactory.getSolver("scipSolver", "deafult");
         solver.solve();
         return "Hello Scip";
